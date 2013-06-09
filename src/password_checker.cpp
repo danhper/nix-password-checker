79fcdffef0ff905973bcabbcdf598a8c47701b4f
tuvistavie <tuvistavie@gmail.com>
2013-06-09 22:05:56 +0900

    Update password checker.

--- src/password_checker.cpp
+++ src/password_checker.cpp
@@ -1,15 +1,25 @@
 #include "password_checker.hpp"
+
 #include <algorithm>
 #include <iterator>
 #include <cstring>
+#include <cstdlib>
+#include <grp.h>
 
 using namespace libconfig;
 
 password_checker::password_checker(const char* path)
 {
   Config config;
-  config.readFile(path);
-  generate_authorized_groups(config);
+  try {
+    config.readFile(path);
+    group_needs_authorization = generate_authorizations(config,
+      "application.authorized-groups", authorized_groups);
+    user_needs_authorization = generate_authorizations(config, "application.authorized-users", authorized_users);
+  } catch(FileIOException& e){
+    group_needs_authorization = false;
+    user_needs_authorization = false;
+  }
 }
 
 password_checker::~password_checker()
@@ -17,23 +27,66 @@ password_checker::~password_checker()
   authorized_groups.clear();
 }
 
-void password_checker::generate_authorized_groups(const Config& config)
+bool password_checker::generate_authorizations(const Config& config,
+  const char* key, std::set<const char*>& authorizations)
 {
-  if(config.exists("application.authorized-groups")) {
-    Setting& auth_groups = config.lookup("application.authorized-groups");
-    if(!auth_groups.isArray()) {
+  if(config.exists(key)) {
+    Setting& auths = config.lookup(key);
+    if(!auths.isArray()) {
       throw new ConfigException();
     } else {
-      for(int i = 0; i < auth_groups.getLength(); i++) {
-        char* s = new char[strlen(auth_groups[i].c_str()) + 1];
-        strcpy(s, auth_groups[i].c_str());
-        authorized_groups.insert(s);
+      for(int i = 0; i < auths.getLength(); i++) {
+        char* s = new char[strlen(auths[i].c_str()) + 1];
+        strcpy(s, auths[i].c_str());
+        authorizations.insert(s);
       }
     }
-    group_needs_authorization = true;
+    return true;
   } else {
-    group_needs_authorization = false;
+    return false;
   }
+
+}
+
+passwd* password_checker::get_user(const char* user_name)
+{
+  if(user == NULL || strcmp(user_name, user->pw_name) != 0)
+  {
+    user = getpwnam(user_name);
+  }
+  return user;
+}
+
+bool password_checker::user_exists(const char* user_name)
+{
+  return get_user(user_name) != NULL;
+}
+
+bool password_checker::is_password_valid(const char* user_name, const char* password)
+{
+  passwd* u = get_user(user_name);
+
+}
+
+bool password_checker::is_user_authorized(const char* user_name)
+{
+  passwd* u = get_user(user_name);
+  group* g = getgrgid(u->pw_gid);
+  if(!is_group_authorized(g->gr_name)) {
+    return false;
+  }
+  if(!user_needs_authorization) {
+    return true;
+  }
+
+  std::set<const char*>::iterator it;
+  for(it = authorized_users.begin(); it != authorized_users.end(); ++it) {
+    if(strcmp(user_name, *it) == 0) {
+      return true;
+    }
+  }
+
+  return false;
 }
 
 bool password_checker::is_group_authorized(const char* group_name)
--- src/password_checker.hpp
+++ src/password_checker.hpp
@@ -1,15 +1,26 @@
 #include <libconfig.h++>
 #include <set>
 
+#include <pwd.h>
+
 class password_checker
 {
 public:
   password_checker(const char* path);
   ~password_checker();
   bool is_group_authorized(const char* group_name);
+  bool is_password_valid(const char* user_name, const char* password);
+  bool user_exists(const char* user_name);
+  bool is_user_authorized(const char* user_name);
 
 private:
   std::set<const char*> authorized_groups;
+  std::set<const char*> authorized_users;
+  passwd* get_user(const char* user_name);
+  passwd* user;
   bool group_needs_authorization;
-  void generate_authorized_groups(const libconfig::Config& config);
+  bool user_needs_authorization;
+
+  bool generate_authorizations(const libconfig::Config& config,
+    const char* key, std::set<const char*>& authorizations);
 };
